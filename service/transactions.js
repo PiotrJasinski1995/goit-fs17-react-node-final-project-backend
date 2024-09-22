ab73c0e7275c44b8f0fd4738e1b7343dd756db28
Sylwia Matusik-Slomka <sylwia.matusik.1994@gmail.com>
2024-09-22 20:47:11 +0200

    Update getTransactionByUser function

--- service/transactions.js
+++ service/transactions.js
@@ -8,10 +8,16 @@ const deleteTransaction = (id) => {
     return Transaction.findByIdAndDelete({ _id: id });
 };
 
-const getTransactionByUser = async(ownerId, transactionType) => {
+const getTransactionByUser = async(ownerId, transactionType = null) => {
     try {
-        const transactions = await Transaction.find({ owner: ownerId, transactionType });
-        return transactions;
+        if (transactionType) {
+            const transactions = await Transaction.find({ owner: ownerId, transactionType });
+            return transactions;
+        } else {
+            const transactions = await Transaction.find({ owner: ownerId });
+            return transactions;
+        }
+
     } catch (error) {
         console.error("Error fetching transactions:", error);
         throw error;
