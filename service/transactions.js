d34ab56573bbd5ae93bcfd49c60f3239660d4474
Sylwia Matusik-Slomka <sylwia.matusik.1994@gmail.com>
2024-09-22 17:48:37 +0200

    Added const getTransactionByUser = async(ownerId) => {

--- service/transactions.js
+++ service/transactions.js
@@ -1,11 +1,21 @@
 const Transaction = require("./schemas/transactions");
 
 const addTransaction = (body) => {
-  return Transaction.create(body);
+    return Transaction.create(body);
 };
 
 const deleteTransaction = (id) => {
-  return Transaction.findByIdAndDelete({ _id: id });
+    return Transaction.findByIdAndDelete({ _id: id });
 };
 
-module.exports = { addTransaction, deleteTransaction };
+const getTransactionByUser = async(ownerId) => {
+    try {
+        const transactions = await Transaction.find({ owner: ownerId });
+        return transactions;
+    } catch (error) {
+        console.error("Error fetching transactions:", error);
+        throw error;
+    }
+}
+
+module.exports = { addTransaction, deleteTransaction, getTransactionByUser };
